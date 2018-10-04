01eaa0b6d39f25865abc39831bf22f23bbc53c39
vzaporteza <38297139+vzaporteza@users.noreply.github.com>
2018-10-04 07:06:37 -0400

    for HW1
    for class 1

---
+++ ps0.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct  3 21:48:53 2018
+
+@author: Mews
+"""
+
+import numpy
+
+x = int(input("Enter a number: "))
+y = int(input("Enter a number: "))
+log = numpy.log2(x)
+log = int(log)
+print ()
+print (x,"**",y,"=",(x ** y))
+print ()
+print ("log","(",x,")" " =", log )
